5b592a37b48a06939f743d191ac9bd5ac5085de3
GabeASM <g.aillapan01@ufromail.cl>
2023-04-28 16:22:23 -0400

    documentacion añadida

--- JS/batalla.js
+++ JS/batalla.js
@@ -2,54 +2,86 @@ const ataques = require('../attacks.json')
 
 
 
+/**
+ * Funcion que sirve como punto de partida para ejecutar los procesos
+ */
 function combate() {
     crearPesonaje()
 }
 
+/**
+ * Funcion utilizada para obtener numeros al azar entre un rango determinado. Esta funcion
+ * es utilizada para dar atributos numericos al azar al momento de crear los personajes
+ * @returns numero decimal al azar entre un rango determinado.
+ */
 function numeroRandom(min, max) {
     return Math.random() * (max - min) + min
 }
+/**
+ * Funcion que escoge un nombre al azar para crear los personajes
+ * Es el primer 'pool' de nombres a utilizar
+ * @returns nombre de tipo string para un personaje
+ */
 
 function nombreRandom() {
     const nombres = ['Juan', 'Diego', 'Pepe', 'Haaland']
     return nombres[Math.floor(Math.random() * nombres.length)];
 }
 
+/**
+ * Funcion que escoge un nombre al azar solo si al crear los personajes los nombres se repite
+ * Funciona como una segunda 'pool' de nombres en caso de que en la primera se repita los nombres
+ * @returns nombre de tipo string para un personaje 
+ */
 function nombreRandom2() {
     const nombres = ['Pedro', 'Davids', 'Cristiano', 'Alexis']
     return nombres[Math.floor(Math.random() * nombres.length)];
 }
 
-function ataqueRandom(ataques) {
-    return ataques[Math.floor(Math.random() * ataques.length)];
-}
-
+/**
+ * Funcion que escoge una clase de las cuales puede ser un personaje y es utilizada en la creacion de estos.
+ * @returns retorna el nombre de una clase al azar.
+ */
 function claseRandom() {
     const clases = ['MAGICIAN', 'KNIGHT', 'WARRIOR', 'FAIRY']
     return clases[Math.floor(Math.random() * clases.length)];
 }
-function ataquesMagicos(){
+
+/**
+ * Funcion que escoge un ataque magico y es utilizada en la creacion de los personajes
+ * @returns retorna un magico fisico escogido al azar del JSON entregado
+ */
+function ataquesMagicos() {
     let ataqueMagico = ataques.filter(ataque => ataque.type == 'MAGIC')
     return ataqueMagico[Math.floor(Math.random() * ataqueMagico.length)];
-
 }
-function ataquesFisicos(){
+
+/**
+ * Funcion que escoge un ataque fisico y es utilizada en la creacion de los personajes
+ * @returns retorna un ataque fisico escogido al azar del JSON entregado
+ */
+function ataquesFisicos() {
     let ataquesFisicos = ataques.filter(ataque => ataque.type == 'PHYSICAL')
     return ataquesFisicos[Math.floor(Math.random() * ataquesFisicos.length)];
 
 }
 
+/**
+ * Fucionalidad que crea los pesonasjes. Esta funcionalidad va creando los personajes con un ciclo
+ * for y utiliza las demas funciones creadas anteriormente para completar los campos de los objetos JavaScript que 
+ * en este caso son los personajes.
+ */
 function crearPesonaje() {
     let personajes = []
     for (let i = 0; i < 2; i++) {
         const vida = Number.parseInt(numeroRandom(100, 200))
-        const velocidad = Number.parseInt(numeroRandom(1 ,10))
+        const velocidad = Number.parseInt(numeroRandom(1, 10))
         let nombreAlAzar = nombreRandom()
-        let claseAlAzar = claseRandom()
+        const claseAlAzar = claseRandom()
         let personaje;
-        if(personajes.length>0){
-            for(let j = 0 ; j < personajes.length ; j++){
-                if(nombreAlAzar == personajes[j].name){
+        if (personajes.length > 0) {
+            for (let j = 0; j < personajes.length; j++) {
+                if (nombreAlAzar == personajes[j].name) {
                     nombreAlAzar = nombreRandom2()
                 }
             }
